02bb261683a9d669e98d2a81bb507c711f3baa68
Vladislav <413022@niuitmo.ru>
2026-03-27 17:20:48 +0300

    refactor: remove not used method from repository

--- src/main/java/ru/urasha/callmeani/blps/repository/TariffOptionRepository.java
+++ src/main/java/ru/urasha/callmeani/blps/repository/TariffOptionRepository.java
@@ -3,7 +3,5 @@ package ru.urasha.callmeani.blps.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.urasha.callmeani.blps.domain.entity.TariffOption;
 
-import java.util.List;
-
 public interface TariffOptionRepository extends JpaRepository<TariffOption, Long> {
 }
